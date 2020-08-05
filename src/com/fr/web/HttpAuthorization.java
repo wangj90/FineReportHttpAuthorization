4025190353544d97a230b4f73a8c649c85025929
wangjun <wang_j90@126.com>
2020-08-05 13:59:30 +0800

    修改为新的登录接口调用

--- src/com/fr/web/HttpAuthorization.java
+++ src/com/fr/web/HttpAuthorization.java
@@ -3,22 +3,18 @@ package com.fr.web;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.w3c.dom.Document;
+import org.apache.http.util.EntityUtils;
 
-import javax.jws.WebService;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URLEncoder;
 
 public class HttpAuthorization extends HttpServlet implements Servlet {
 
@@ -29,13 +25,12 @@ public class HttpAuthorization extends HttpServlet implements Servlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("fr_username");
         String password = request.getParameter("fr_password");
-        String url = "http://10.31.8.43/UserAccount/Service1.asmx/Validate";
-        Boolean result = doGetSoap1_1(url + "?name=" + URLEncoder.encode(username, "UTF-8") + "&pwd=" + URLEncoder.encode(password, "UTF-8"));
+        String url = "https://co-op.otiz.com.cn:34000/api/mHR/Login";
+        Boolean result = requestLogin(url, "{\"UserID\":\"" + username + "\",\"PassWord\":\"" + password + "\",\"AppCode\":\"OTIZPortal\"}");
         response.getWriter().write(result.toString());
     }
 
-    private static Boolean doGetSoap1_1(String url) {
-        String retStr = "";
+    private static Boolean requestLogin(String url, String params) throws IOException {
         HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
         CloseableHttpClient closeableHttpClient = httpClientBuilder.build();
         RequestConfig requestConfig = RequestConfig
@@ -43,23 +38,25 @@ public class HttpAuthorization extends HttpServlet implements Servlet {
                 .setSocketTimeout(30000)
                 .setConnectTimeout(30000)
                 .build();
-        HttpGet httpGet = new HttpGet(url);
-        httpGet.setConfig(requestConfig);
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setConfig(requestConfig);
         try {
-            httpGet.setHeader("Content-Type", "text/xml;charset=UTF-8");
-            CloseableHttpResponse response = closeableHttpClient.execute(httpGet);
-            HttpEntity httpEntity = response.getEntity();
-            if (null != httpEntity) {
-                InputStream in = httpEntity.getContent();
-                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-                DocumentBuilder builder = factory.newDocumentBuilder();
-                Document document = builder.parse(in);
-                retStr = document.getDocumentElement().getTextContent();
+            httpPost.setHeader("Content-Type", "application/json;charset=UTF-8");
+            httpPost.setEntity(new StringEntity(params, "utf-8"));
+            try (CloseableHttpResponse response = closeableHttpClient.execute(httpPost)) {
+                HttpEntity httpEntity = response.getEntity();
+                if (null != httpEntity) {
+                    String str = EntityUtils.toString(httpEntity);
+                    return str.startsWith("{\"code\":20000");
+                }
+            } catch (Exception e) {
+                return false;
             }
-            closeableHttpClient.close();
         } catch (Exception e) {
             return false;
+        } finally {
+            closeableHttpClient.close();
         }
-        return Boolean.valueOf(retStr);
+        return false;
     }
 }
